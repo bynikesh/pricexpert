8ae388cebc55034ce2440317f65b28dd826e9d20
nikesh <palpalikta@gmail.com>
2016-05-17 17:41:27 +0545

    asp general files created

---
+++ src/pricexpert/Controllers/web/AppController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pricexpert.Controllers.web
+{
+    public class AppController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Conatct()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
